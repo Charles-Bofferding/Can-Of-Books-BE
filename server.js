72845b723cb618737aad7f06c14250b5d196a9cf
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-05-25 16:56:46 -0700

    finished

--- server.js
+++ server.js
@@ -10,15 +10,18 @@ const jwksClient = require('jwks-rsa');
 //Setting up running env
 const app = express();
 app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 //Next two things come from "the docs" except they const instead of var
 // jsonwebtoken doc - https://www.npmjs.com/package/jsonwebtoken
 const client = jwksClient({
-  jwksUri: 'https://dev-o879tgum.us.auth0.com/.well-known/jwks.json',
+  jwksUri: 'https://grandvizier.us.auth0.com/.well-known/jwks.json',
 });
+// grandvizier.us.auth0.com
 
 function getKey(header, callback) {
+  // console.log(header);
   client.getSigningKey(header.kid, function (err, key) {
     const signingKey = key.publicKey || key.rsaPublicKey;
     callback(null, signingKey);
@@ -33,13 +36,15 @@ function getKey(header, callback) {
 // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 app.get('/test', (request, response) => {
 
-  console.log(request);
+  // console.log(request);
+  // console.log(request.headers);
+  console.log(request.header.name);
 
   //From the request get the json web token from where we hope it is
   const token = request.headers.authorization.split(' ')[1];
-
-  //Verify the token, use the getKey from the docs
-  //gets an error or user back
+    console.log(token);
+  // Verify the token, use the getKey from the docs
+  // gets an error or user back
   jwt.verify(token, getKey, {}, (err, user) => {
 
     //Check for an error
